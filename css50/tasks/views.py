76874a0ab050818033b91b002834678170a3dbd6
derick-droid <derrickokinda9@gmail.com>
2021-12-16 15:06:53 +0300

    adding validation

--- css50/tasks/views.py
+++ css50/tasks/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, render_to_response
-
+from django import forms
 # Create your views here.
 
+# generating django html forms 
+class NewTask(forms.Form):
+    tasks = forms.CharField(label = "New activity")
+    priority = forms.IntegerField(label="priority", min_value=1, max_value= 10)
+    
+# list of activities
 def tasks(request):
     tasks = ["python", "research", "attachment report"]
 
@@ -9,5 +15,18 @@ def tasks(request):
         "tasks":tasks
     })
     
+# adding more activities 
 def add(request):
-    return render(request,"tasks/add.html", {})
+    if request.method == "POST":
+        form = NewTask(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+        else:
+            return render(request, "tasks/add.html", {
+                "forms": form
+            })
+            
+    return render(request,"tasks/add.html", {
+        "forms":NewTask()
+    })
